61a4059b103784f494867830edbdc07716718df6
Lordede <44034930+Lordede@users.noreply.github.com>
2022-05-26 08:52:34 +0200

    Added Musclegroup Table

--- SqlScripts/CreateExercise.sql
+++ SqlScripts/CreateExercise.sql
@@ -1,8 +1,9 @@
 CREATE Table exercises(
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    muscleGroup varchar(255) NOT NULL,
+    musclegroupId int,
     trainingId int NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (trainingId) REFERENCES trainings(id)
+    FOREIGN KEY (musclegroupId) REFERENCES musclegroups(id)
    )
---
+++ SqlScripts/CreateMusclegroup.sql
@@ -0,0 +1,5 @@
+CREATE Table musclegroups(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+   )
