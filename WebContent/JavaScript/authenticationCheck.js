b4766a8bcc35062be1da00fed8b8a0b3a62b0ef0
Ainges <hubertusseitz@gmail.com>
2022-05-30 10:24:57 +0200

    added to authenticationCheck.js: redirect if not logged in

--- WebContent/JavaScript/authenticationCheck.js
+++ WebContent/JavaScript/authenticationCheck.js
@@ -6,10 +6,8 @@ function authenticationCheck(userData){
 	
 	if (userData === "") { //wird noch nicht richtig ausgewertet
 		
-		
-		document.getElementsByTagName('BODY')[0].innerHTML = ''; // https://social.msdn.microsoft.com/Forums/en-US/29f4e7ce-ab58-43e3-a0fa-0b34f9967117/clear-body-with-javascript?forum=aspwebformsdata
-		console.log("Keine UserData gefunden!")
 		alert("Bitte einloggen!")
+		window.location.replace("login.html");   
 	}
 	else{
 		console.log("UserData wurde gefunden!")
