95c31ef5288ce722d33ba988ccdac7752b2afc88
Ainges <hubertusseitz@gmail.com>
2022-06-05 12:49:18 +0200

    Satz hinzufügen funktioniert

--- WebContent/JavaScript/CreateExercise.js
+++ WebContent/JavaScript/CreateExercise.js
@@ -1,48 +1,4 @@
 "use strict";
-
-/*Creates the exercise with table dynamically */
-function insertExercise() {
-    var counter = 1;
-    var insertableBlock;
-    var setData = "${set}";
-    var exerciseName = "${exercise.name}";
-    var muscleGroup = "${exercise.muscleGroup"
-    var img = "${exercise.img}"
-    var parent = document.getElementById("exerciseContainer");
-
-    insertableBlock = document.createElement('div');
-    insertableBlock.setAttribute("id", "exercise" + counter(counter))
-    insertableBlock.innerHTML = '<h2>' + exerciseName + '</h2>' +
-        '<h3>' + '(' + muscleGroup + ')' + '</h3>' +
-        loadTable;
-    parent.appendChild(insertableBlock);
-
-
-}
-var setData = []; //Daten vom Set
-
-function loadTable() //Content injetion
-{
-    var counter = 3;
-    var exerciseTable = document.createElement('table');
-    var parent = document.getElementById('exercise');
-
-    exerciseTable.setAttribute("id", "exerciseTable" + counter(counter)); //get specific Table
-    let dataHtml = ''; //append html data as string
-
-    window.onload = () =>  //initialisierung des windowobjects wobei die tableData (aus html) geladen wird
-
-    {
-        loadTableData(setData);
-    }
-
-    for (let set of setData) {
-        dataHtml += '<tr><td>${set.no}</td><td>${set.rep}</td><td>${set.kg}</tr>';
-    }
-    tableBody.innerHTML = dataHtml; //initialisierung default Belegung DOM
-    parent.appendChild('exercise');
-}
-
 // author: Hubertus
 
 /*
@@ -82,8 +38,7 @@ function addExercise() {
     var kgtd = document.createElement("td");
     var whdtd = document.createElement("td");
     var checktd = document.createElement("td");
-    satztd.innerHTML = counter;
-    counter++;
+    satztd.innerHTML = "#"; //TODO: Satznummer hinzufügen!
 
     //Feld um Gewicht einzutragen
     var kg = document.createElement("input");
@@ -123,7 +78,9 @@ function addSet() {
     let NameOfcallingExercise = this.parentNode.getAttribute("name");      // Name der Übung
 
     /*
-    Das Nachfolgende muss irgendwie auch elleganter gehen aber für den Moment funktioniert es
+    Das Nachfolgende muss irgendwie auch eleganter gehen aber für den Moment funktioniert es
+
+    INFO: Über das Attribut "name" des jeweiligen Article weiß der Button wo er das Set hinzufügen soll! 
     ---
     */
 
@@ -148,7 +105,7 @@ function addSet() {
     var checktd = document.createElement("td");
 
 
-    satztd.innerHTML = "#"; //TODO: Einfügen
+    satztd.innerHTML = "#"; //TODO: Satznummer hinzufügen!
 
     //Feld um Gewicht einzutragen
     var kg = document.createElement("input");
