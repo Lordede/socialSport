cf8cb9c7d93f38ebe0b004dc9e6d9d2c5d15c5eb
Ainges <hubertusseitz@gmail.com>
2022-06-07 22:22:18 +0200

    implementation done
    implementation done

--- WebContent/JavaScript/registration.js
+++ WebContent/JavaScript/registration.js
@@ -7,72 +7,86 @@
  */
 
 
-function emailAvailable(){
-    
+function emailAvailable() {
+
     var searchURL = "../CheckAvailabilityServlet"; //TODO: Hinzufügen
     var email = document.getElementById("emailInput").value;
-    
-    if(email != null && email.length>0){
+
+    if (email != null && email.length > 0) {
         searchURL += "?email=" + encodeURIComponent(email); //Erstellung des Querry Strings
     }
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.responseType = "json";
 
 
-        xmlhttp.addEventListener("load", function(){    //Ausführen einer anonymen Funktion, sobald eine Antwort vom Server gekommen ist
+    xmlhttp.addEventListener("load", function () {    //Ausführen einer anonymen Funktion, sobald eine Antwort vom Server gekommen ist
         var availability = xmlhttp.response;            // JSON to JS Object!
-        
-            if(availability.email === "true"){
-                
-                console.log("E-Mail: Verfügabr!")
-            }
-            else{
-                console.log("E-Mail: NICHT Verfügbar!");
-            
-            }
+        var check = document.getElementById("emailcheck");
 
-        });
+        if (!email.includes("@")) { //Falls noch kein @ eingegeben wurde soll auch noch kein "❌" angezeigt werden
+            check.innerHTML = "";
+        }
+        else if (availability.email === "true") {
 
-        xmlhttp.open("GET", searchURL, true)
-        xmlhttp.send(); //setRequestHeader() nur bei Post benötigt
-            
-     
+            console.log("E-Mail: Verfügabr!")
+            check.innerHTML = "✅";
 
+        }
+        else {
+            console.log("E-Mail: NICHT Verfügbar!");
+            check.innerHTML = "❌";
+        }
 
 
+    });
+
+    xmlhttp.open("GET", searchURL, true)
+    xmlhttp.send(); //setRequestHeader() nur bei Post benötigt
 
 
-    
+
+
+
+
+
+
 
 }
 
-function usernameAvailable(){
+function usernameAvailable() {
 
     var searchURL = "../CheckAvailabilityServlet"; //TODO: Hinzufügen
     var username = document.getElementById("usernameInput").value;
-    
-    if(username != null && username.length>0){
+    var check = document.getElementById("usernamecheck");
+
+    if (username != null && username.length > 0) {
         searchURL += "?username=" + encodeURIComponent(username); //Erstellung des Querry Strings
     }
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.responseType = "json";
 
 
-        xmlhttp.addEventListener("load", function(){    //Ausführen einer anonymen Funktion, sobald eine Antwort vom Server gekommen ist
+    xmlhttp.addEventListener("load", function () {    //Ausführen einer anonymen Funktion, sobald eine Antwort vom Server gekommen ist
         var availability = xmlhttp.response;            // JSON to JS Object!
-        
-            if(availability.username === "true"){
-                
-                console.log("username: Verfügabr!")
-            }
-            else{
-                console.log("username: NICHT Verfügbar!");
-            
-            }
 
-        });
+        if (username.length < 2) {
+            check.innerHTML = "";
+        }
 
-        xmlhttp.open("GET", searchURL, true)
-        xmlhttp.send(); //setRequestHeader() nur bei Post benötigt
+        else if (availability.username === "true") {
+
+            console.log("username: Verfügabr!")
+            check.innerHTML = "✅";
+        }
+        else {
+            console.log("username: NICHT Verfügbar!");
+            check.innerHTML = "❌";
+
+        }
+
+    });
+
+    xmlhttp.open("GET", searchURL, true)
+    xmlhttp.send(); //setRequestHeader() nur bei Post benötigt
 
 }
