a7ac70ec27ef956c733fed02687cbdfc60ef3793
Ainges <hubertusseitz@gmail.com>
2022-06-28 22:12:15 +0200

    js angepasst

--- WebContent/JavaScript/Leaderboard.js
+++ WebContent/JavaScript/Leaderboard.js
@@ -1,43 +1,8 @@
-//@author Hubertus Seitz
-// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_number
-//----------------------------------------
-// function sortTable() {
-//     var table, rows, switching, i, x, y, shouldSwitch;
-//     table = document.getElementById("leaderboardTable");
-//     switching = true;
-//     /*Make a loop that will continue until
-//     no switching has been done:*/
-//     while (switching) {
-//       //start by saying: no switching is done:
-//       switching = false;
-//       rows = table.rows;
-//       /*Loop through all table rows (except the
-//       first, which contains table headers):*/
-//       for (i = 1; i < (rows.length - 1); i++) {
-//         //start by saying there should be no switching:
-//         shouldSwitch = false;
-//         /*Get the two elements you want to compare,
-//         one from current row and one from the next:*/
-//         x = rows[i].getElementsByTagName("TD")[0];
-//         y = rows[i + 1].getElementsByTagName("TD")[0];
-//         //check if the two rows should switch place:
-//         if (Number(x.innerHTML) < Number(y.innerHTML)) {
-//           //if so, mark as a switch and break the loop:
-//           shouldSwitch = true;
-//           break;
-//         }
-//       }
-//       if (shouldSwitch) {
-//         /*If a switch has been marked, make the switch
-//         and mark that a switch has been done:*/
-//         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-//         switching = true;
-//       }
-//     }
-//   }
-  //----------------------------------------
+/**
+ * @author Hubertus Seitz
+ **/
 
-  function loadLeaderboard(){ //laden des Leaderboards
+  function loadLeaderboard(){ //laden des Leaderboards über einen  AJAX-Aufruf
 
     var url = "../JoinServlet?getLeaderboard";
     
@@ -46,7 +11,6 @@
     xmlhttp.addEventListener("load" , function(){
 
         var leaderboard = JSON.parse(xmlhttp.response);       // js Objekt aus jsonfile
-        //var table = document.getElementById("leaderboardTable").children[0].children[0]; //Damit auf das tr verwiesen wird
         var headrow = document.getElementById("Headrow").parentElement; //Damit auf das tr verwiesen wird
 
         if (leaderboard === null || leaderboard.length === 0) {
